2a7dd1f470b37019d0400c8c06c2082869bb6fbd
Paul van Assen <source@paules.nl>
2013-09-08 15:59:11 +0200

    Fixed lock Fixed port Fixed array Added extra debug info

--- software/bash-based/push.sh
+++ software/bash-based/push.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 PORT=4
-PUSH_TO=( xively )
-
+PUSH_TO=('sense' 'xively');
 while true
 do
-        echo "Aquiring lock"
+        echo "Aquiring lock for push"
         while [ -e /tmp/read-lock ]
         do
                 sleep 1s
         done
-        touch /tmp/lock
-        echo "Got lock"
-        filevalue=`cat /tmp/port-$index 2> /dev/null`
+        touch /tmp/read-lock
+        echo "Got lock for push"
+        filevalue=`cat /tmp/port-$PORT 2> /dev/null`
         VALUE=${filevalue:-0}
-        for service in $PUSH_TO
+        for service in "${PUSH_TO[@]}"
         do
                 TMP_FILE=/tmp/filled-$service.json
                 source $service.conf
@@ -22,10 +21,10 @@ do
                         eval echo "$line"
                 done < $service.json > $TMP_FILE
                 curl_cmd=`eval echo "$CURL"`
-                eval $curl_cmd
+                eval $curl_cmd 1> /dev/null
         done
-        echo "Releasing lock"
-        rm -rf /tmp/port-$index
-        rm -rf /tmp/lock
+        echo "Releasing lock for push"
+        rm -rf /tmp/port-$PORT
+        rm -rf /tmp/read-lock
         sleep 60
 done
